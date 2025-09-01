79129e926f277e0f8671db8539d97e5c92ab3243
Vivek Rohilla <vivekrohilla@Viveks-MacBook-Air.local>
2025-09-01 23:16:00 +0530

    added

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "74282b2c-f351-49d2-8fc1-345ec077bafb"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-storage-demo"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "mystorageacctdemo123" # globally unique hona chahiye
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
