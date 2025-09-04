9fdad870f62f457cf000009ecca71b78b044e2d7
Vivek Rohilla <vivekrohilla@Viveks-MacBook-Air.local>
2025-09-04 22:08:05 +0530

    new added

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ terraform {
       version = ">3.0"
     }
   }
-  # backend "azurerm" {
+  backend "azurerm" {
     
-  #   resource_group_name = "pardeeptestrg"              # Can also be set via `ARM_CLIENT_ID` environment variable.
-  #   storage_account_name = "pardeeptestsg"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-  #   container_name       = "pardeepcontainertest"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-  #   key                  = "vivek.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  # }
+    resource_group_name = "pardeeptestrg"              # Can also be set via `ARM_CLIENT_ID` environment variable.
+    storage_account_name = "pardeeptestsg"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "pardeepcontainertest"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "vivek.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 
